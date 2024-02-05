cfd831d6c79bc2d6d0515538ef4d462b19c4a47c
masatodesudesu <masato.sk01@gmail.com>
2024-02-05 09:03:59 +0900

    First commit to keep code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,3 +18,19 @@ const config: Config = {
   plugins: [],
 }
 export default config
+
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-rgba': 'rgba(0, 0, 0, 0.3)',
+      },
+    },
+  },
+  plugins: [],
+}
